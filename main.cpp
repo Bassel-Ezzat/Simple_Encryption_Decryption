df10de766582899081034d90475e11807ab64b7f
Bassel Ezzat <Bassel.a.ezzat@gmail.com>
2025-05-15 23:21:56 +0300

    Added Caesar cipher decryption choice in int main and did some slight modifications

--- main.cpp
+++ main.cpp
@@ -7,20 +7,42 @@ string CaesarEnc (string msg , int key);
 string CaesarDec (string dec , int key);
 
 int main () {
-    int Enc_choice;
-    cout << "Choose The Encryption method :" << endl
-    << "1- Caeser Cipher" << endl << "2- XOR Cipher" << endl
-    << "3- Vigenere Cipher" << endl;
-    cin >> Enc_choice;
-    cin.ignore();
-    if (Enc_choice == 1) {
-        string msg ;
-        int key;
-        cout << "Enter the text you want to encrypt:" ;
-        getline(cin , msg);
-        cout << "Enter the encryption key:";
-        cin >> key;
-        cout << CaesarEnc(msg , key);
+    int EncorDec;
+    cout << "1- Encrypt" << endl << "2- Decrypt" << endl;
+    cin >> EncorDec;
+    if (EncorDec == 1) {
+        int Enc_choice;
+        cout << "Choose The Encryption method :" << endl
+        << "1- Caesar Cipher" << endl << "2- XOR Cipher" << endl
+        << "3- Vigenere Cipher" << endl;
+        cin >> Enc_choice;
+        cin.ignore();
+        if (Enc_choice == 1) {
+            string msg ;
+            int key;
+            cout << "Enter the text you want to encrypt:" ;
+            getline(cin , msg);
+            cout << "Enter the encryption key:";
+            cin >> key;
+            cout << CaesarEnc(msg , key);
+        }
+    }
+    else if (EncorDec == 2) {
+        int Dec_choice;
+        cout << "Choose Which Cipher You Want To Decrypt:" << endl
+        <<"1- Caesar Cipher" << endl << "2- XOR Cipher" << endl
+        << "3- Vigenere Cipher" << endl;
+        cin >> Dec_choice;
+        cin.ignore();
+        if (Dec_choice == 1) {
+            int key ;
+            string msg;
+            cout << "Enter the text you want to Decrypt:" ;
+            getline(cin , msg);
+            cout << "Enter the Decryption key:";
+            cin >> key;
+            cout << CaesarDec(msg , key);
+        }
     }
 
 }
@@ -43,7 +65,7 @@ int main () {
 
 
 
-// Caeser Cipher Encryption
+// Caesar Cipher Encryption
 string CaesarEnc (string msg , int key) {
     for (int i = 0; i < msg.length(); i++) {
         if (isupper(msg[i])) {
@@ -57,7 +79,7 @@ string CaesarEnc (string msg , int key) {
     return msg;
 }
 
-// Caeser Cipher Decryption
+// Caesar Cipher Decryption
 string CaesarDec (string dec , int key) {
     for (int i = 0; i < dec.length(); i++) {
         if (isupper(dec[i])) {
