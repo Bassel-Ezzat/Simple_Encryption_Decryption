f11d651dab1b440169645bd5c88194693f999fcd
Bassel Ezzat <Bassel.a.ezzat@gmail.com>
2025-05-18 21:01:52 +0300

    Fixed CaesarEnc and CaesarDec logic

--- main.cpp
+++ main.cpp
@@ -212,10 +212,10 @@ string CaesarEnc (string msg , int key) {
 
     for (int i = 0; i < msg.length(); i++) {
         if (isupper(msg[i])) {
-            msg[i] = (( msg[i] - 'A' + key ) % 26) + 'A' ;
+            msg[i] = ((msg[i] - 'A' + key + 26) % 26) + 'A';
         }
         else if (islower(msg[i])) {
-            msg[i] = (( msg[i] - 'a' + key ) % 26) + 'a';
+            msg[i] = ((msg[i] - 'a' + key + 26) % 26) + 'a';
         }
 
     }
@@ -228,10 +228,10 @@ string CaesarDec (string dec , int key) {
 
     for (int i = 0; i < dec.length(); i++) {
         if (isupper(dec[i])) {
-            dec[i] = (( dec[i] - 'A' - key) % 26) + 'A' ;
+            dec[i] = (((dec[i] - 'A' - key + 26) % 26) + 'A');
         }
         else if (islower(dec[i])) {
-            dec[i] = (( dec[i] - 'a' - key) % 26) + 'a' ;
+            dec[i] = (((dec[i] - 'a' - key + 26) % 26) + 'a');
         }
     }
     return dec;
